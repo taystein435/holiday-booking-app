99ecbbba01a1ccf33a25f1b96ea934025fd7f3b8
taystein435 <taystein435@gmail.com>
2025-05-04 19:01:47 +0100

    Add account, bookings, and notifications components

---
+++ holiiday-booking/app/(tabs)/bookings.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const bookings = () => {
+  return (
+    <View>
+      <Text>bookings</Text>
+    </View>
+  )
+}
+
+export default bookings
