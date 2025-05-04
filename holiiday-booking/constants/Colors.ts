3ea975dab0b13d0332f09a8eb1cb4643876c1d1f
taystein435 <taystein435@gmail.com>
2025-05-04 21:34:06 +0100

    Update tab layout and color scheme for improved UI consistency

--- holiiday-booking/constants/Colors.ts
+++ holiiday-booking/constants/Colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#00d3d8 ';
 const tintColorDark = '#fff';
 
 export const Colors = {
@@ -11,8 +11,8 @@ export const Colors = {
     text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#00d3d8 ',
+    tabIconDefault: '#00d3d8 ',
     tabIconSelected: tintColorLight,
   },
   dark: {
