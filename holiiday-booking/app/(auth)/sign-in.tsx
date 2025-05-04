53a5336d2daea1e8e663a516af6fc5b275bf6d95
taystein435 <taystein435@gmail.com>
2025-05-04 22:37:08 +0100

    Add video background to onboarding screen and update dependencies

--- holiiday-booking/app/(auth)/sign-in.tsx
+++ holiiday-booking/app/(auth)/sign-in.tsx
@@ -1,66 +1,18 @@
-import { useSignIn } from '@clerk/clerk-expo'
-import { Link, useRouter } from 'expo-router'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+// app/sign-in.tsx
+import { SignIn } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
+import { useEffect } from 'react'
+import { useAuth } from '@clerk/clerk-expo'
 
-export default function Page() {
-  const { signIn, setActive, isLoaded } = useSignIn()
+export default function SignInScreen() {
   const router = useRouter()
+  const { isSignedIn } = useAuth()
 
-  const [emailAddress, setEmailAddress] = React.useState('')
-  const [password, setPassword] = React.useState('')
-
-  // Handle the submission of the sign-in form
-  const onSignInPress = async () => {
-    if (!isLoaded) return
-
-    // Start the sign-in process using the email and password provided
-    try {
-      const signInAttempt = await signIn.create({
-        identifier: emailAddress,
-        password,
-      })
-
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
-      if (signInAttempt.status === 'complete') {
-        await setActive({ session: signInAttempt.createdSessionId })
-        router.replace('/')
-      } else {
-        // If the status isn't complete, check why. User might need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2))
-      }
-    } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2))
+  useEffect(() => {
+    if (isSignedIn) {
+      router.replace('/')
     }
-  }
+  }, [isSignedIn])
 
-  return (
-    <View>
-      <Text>Sign in</Text>
-      <TextInput
-        autoCapitalize="none"
-        value={emailAddress}
-        placeholder="Enter email"
-        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-      />
-      <TextInput
-        value={password}
-        placeholder="Enter password"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <TouchableOpacity onPress={onSignInPress}>
-        <Text>Continue</Text>
-      </TouchableOpacity>
-      <View style={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-        <Link href="/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </View>
-    </View>
-  )
+  return <SignIn />
 }
